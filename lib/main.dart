90b4f0d16899b2f06fcbc97432e9de724997a177
Oxana Kostikova <lereenadem@yandex.ru>
2023-11-05 13:00:28 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'news/main_page.dart';
+import 'theme.dart';
+
+void main() {
+  runApp(const EclipseTestApp());
+}
+
+class EclipseTestApp extends StatelessWidget {
+  const EclipseTestApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Eclipse test',
+      theme: AppTheme.lightTheme(),
+      home: const Material(child: MainPage()),
+    );
+  }
+}
