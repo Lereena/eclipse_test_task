dfef6ba27f6d839f178eb464cfabba6057efd9b3
Oxana Kostikova <lereenadem@yandex.ru>
2023-11-05 21:08:53 +0100

    Add metadata

--- lib/repositories/repositories.dart
+++ lib/repositories/repositories.dart
@@ -1,5 +1,3 @@
-export './albums/abstract_albums_repository.dart';
-export './albums/albums_repository.dart';
 export './photos/abstract_photos_repository.dart';
 export './photos/photos_repository.dart';
 export './users/abstract_users_repository.dart';
